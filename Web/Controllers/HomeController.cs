f032f72129766725ced73242ee947a2d224a90ae
Abdelkarim Abdallah <abdelkarim.abdallah@brunata-hamburg.de>
2020-04-29 00:24:05 +0200

    Get Data from the DB

--- Web/Controllers/HomeController.cs
+++ Web/Controllers/HomeController.cs
@@ -2,15 +2,31 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Core.Entities;
+using Core.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using Web.ViewModels;
 
 namespace Web.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IUnitOfWork<Owner> _Owner;
+        private readonly IUnitOfWork<PortfolioItem> _Portfolio;
+
+        public HomeController(IUnitOfWork<Owner> owner, IUnitOfWork<PortfolioItem> portfolio)
+        {
+            _Owner = owner;
+            _Portfolio = portfolio;
+        }
         public IActionResult Index()
         {
-            return View();
+            var model = new HomeViewModel
+            {
+                Owner = _Owner.Entity.GetAll().First(),
+                PortfolioItems = _Portfolio.Entity.GetAll().ToList()
+            };
+            return View(model);
         }
     }
 }
